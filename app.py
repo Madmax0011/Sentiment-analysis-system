415971ca620e99e07c3f59928263f796ccf668ec
Shriyansh Nautiyal <shriyanshnauti@gmail.com>
2024-07-25 19:01:57 +0100

    Added HTML form and sentiment analysis

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 
 app = Flask(__name__)
@@ -9,16 +9,15 @@ vectorizer = joblib.load('vectorizer.pkl')
 
 @app.route('/')
 def home():
-    return "Welcome to the Sentiment Prediction API!"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
-    text = data['text']
+    text = request.form['text']
     transformed_text = vectorizer.transform([text])
     prediction = model.predict(transformed_text)
     sentiment = 'Positive' if prediction[0] == 1 else 'Negative'
-    return jsonify({'sentiment': sentiment})
+    return render_template('index.html', sentiment=sentiment)
 
 if __name__ == '__main__':
     app.run(debug=True)
